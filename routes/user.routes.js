55162eda805243e899cf8fc160d165a40f93ad3e
claratesmon <claraperezmontes@gmail.com>
2023-12-15 17:36:01 +0100

    cloudinary image upload

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const User = require("../models/User.model");
-// const fileUploader = require("../config/cloudinary.config");
+const fileUploader = require("../config/cloudinary.config");
 
 router.get("/:userId", (req, res, next) => {
     const id = req.params.userId;
@@ -34,6 +34,20 @@ router.put("/edituser",(req, res, next) => {
         console.log("updateduser",updatedUser);
     })
     .catch((err) => (err))
+});
+
+router.post("/upload", fileUploader.single("profilePicture"), (req, res, next) => {
+    console.log("file is: ", req.file)
+
+   if (!req.file) {
+       next(new Error("No file uploaded!"));
+       return;
+   }
+
+   // Get the URL of the uploaded file and send it as a response.
+   // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+
+   res.json({ fileUrl: req.file.path });
 });
 
 module.exports = router;
