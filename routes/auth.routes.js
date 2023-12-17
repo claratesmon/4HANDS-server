4fb269987a425a697f14b1cae69c5267c037d97f
claratesmon <claraperezmontes@gmail.com>
2023-12-17 10:57:49 +0100

    dependecies update

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // ℹ️ Handles password encryption
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 
 // ℹ️ Handles password encryption
 const jwt = require("jsonwebtoken");
